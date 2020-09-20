79b87a368e122778273cbda8278620d11f851c17
Patrick Monteiro <locomontero33@hotmail.com>
2020-09-20 11:15:23 -0300

    Update!
    09/20/20

--- requests.py
+++ requests.py
@@ -1,26 +1,21 @@
 import requests
 import redis
-import sys
 
+redis_config = {
+    'host': '<host>',
+    'port': 6379,
+    'db': 0
+}
 
-def redis_connect() -> redis.client.Redis:
-    try:
-        client = redis.Redis(
-            host="localhost",
-            port=6379,
-            password="",
-            db=0,
-            socket_timeout=5,
-        )
-        ping = client.ping()
-        if ping is True:
-            return client
-    except redis.AuthenticationError:
-        print("AuthenticationError")
-        sys.exit(1)
+r = redis.StrictRedis(**redis_config)
 
-    client = redis_connect()
-
+redis_key = '<requests_params>_<time>'
+data = json.dumps(vacancies_data)   #You have to serialize the data to json!
+r.setex(
+    redis_key,
+    60*60,        # time to store data
+    data
+)
 
 def get(url, parameters=None):
     """ Fetches an URL and returns the response """
@@ -42,5 +37,4 @@ def delete(url, parameters=None, data=None):
     """ Requests the deletion of an resource and returns the response """
     return requests.delete(url, params=parameters, data=data).json()
 
-print(requests.txt)
 
